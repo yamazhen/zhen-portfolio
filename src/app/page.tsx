abb5e958f6d6957c5bb848f721bde1458864fa61
yamazhen <bowxyt@gmail.com>
2025-08-17 18:11:58 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,19 @@ export default function Home() {
 
 	return (
 		<>
-			<PageSection first className="items-center">
-				<div className="flex items-center gap-6 lg:gap-10 max-lg:flex-col py-8 max-lg:items-start justify-center">
-					<div className="flex flex-col gap-8 max-lg:gap-4 max-md:gap-2 max-lg:order-1">
-						<small className="max-lg:text-xs"><span className="text-accent">~/portfolio</span> $ cat intro.txt</small>
-						<Typewriter text="hi, bowen here. 👋" className="text-accent max-lg:text-4xl max-md:text-base text-5xl font-bold whitespace-nowrap" />
-						<p className="text-muted max-md:text-xs">{age}yo software engineer from Malaysia</p>
-						<p className="max-lg:text-sm">Fullstack by trade, I solve problems with code and deploy solutions with spite.</p>
-						<div className="flex items-center gap-4 text-sm max-md:text-xs">
-							<a href="/file/resume.pdf" download className="text-background bg-accent hover:bg-accent-dim max-md:px-3 max-md:py-1.5 transition-all duration-500 rounded-sm px-6 py-3 hover:shadow-accent-dim/50 hover:shadow-[0_0_30px] cursor-pointer">Resume</a>
-							<Link href={"/projects"} className="border border-border bg-background px-6 py-3 rounded-sm max-md:px-3 max-md:py-1.5 hover:border-accent glass-hover duration-500 transition-colors cursor-pointer">Projects</Link>
-							<a href="https://github.com/yamazhen" target="_blank" rel="noopener noreferrer"><Github strokeWidth={1} className="text-muted hover:text-accent transition-colors duration-500 cursor-pointer max-md:h-4 max-md:w-4" /></a>
-							<a href="https://linkedin.com/in/yamazhen" target="_blank" rel="noopener noreferrer"><Linkedin strokeWidth={1} className="text-muted hover:text-accent transition-colors duration-500 cursor-pointer max-md:h-4 max-md:w-4" /></a>
-							<a href="mailto:bowxyt@gmail.com"><LucideMail strokeWidth={1} className="text-muted hover:text-accent transition-colors duration-500 cursor-pointer max-md:h-4 max-md:w-4" /></a>
-						</div>
-					</div>
-					<div className="flex items-center w-full h-full justify-start max-md:w-48 md:w-96">
-						<Image src="/img/me.JPG" alt="picture of me" width={1000} height={1000} className="rounded-xl md:h-64 md:w-64 lg:w-full lg:h-full" />
+			<PageSection first className="gap-8 md:flex-row md:items-center">
+				<Image src="/img/me.JPG" alt="picture of me" width={1000} height={1000} className="rounded-xl max-md:w-48 md:w-64 md:h-64 md:order-1" />
+				<div className="flex flex-col justify-center md:gap-8 gap-2">
+					<small className=""><span className="text-accent">~/portfolio</span> $ cat intro.txt</small>
+					<Typewriter text="hi, bowen here. 👋" className="text-accent md:text-4xl text-xl text-nowrap" />
+					<p className="text-muted text-xs md:text-base">{age}yo software engineer from Malaysia</p>
+					<p className="max-md:text-sm">Fullstack by trade, I solve problems with code and deploy solutions with spite.</p>
+					<div className="flex items-center gap-4 text-sm max-md:text-xs">
+						<a href="/file/resume.pdf" download className="text-background bg-accent hover:bg-accent-dim max-md:px-3 max-md:py-1.5 transition-all duration-500 rounded-sm px-6 py-3 hover:shadow-accent-dim/50 hover:shadow-[0_0_30px] cursor-pointer">Resume</a>
+						<Link href={"/projects"} className="border border-border bg-background px-6 py-3 rounded-sm max-md:px-3 max-md:py-1.5 hover:border-accent glass-hover duration-500 transition-colors cursor-pointer">Projects</Link>
+						<a href="https://github.com/yamazhen" target="_blank" rel="noopener noreferrer"><Github strokeWidth={1} className="text-muted hover:text-accent transition-colors duration-500 cursor-pointer max-md:h-4 max-md:w-4" /></a>
+						<a href="https://linkedin.com/in/yamazhen" target="_blank" rel="noopener noreferrer"><Linkedin strokeWidth={1} className="text-muted hover:text-accent transition-colors duration-500 cursor-pointer max-md:h-4 max-md:w-4" /></a>
+						<a href="mailto:bowxyt@gmail.com"><LucideMail strokeWidth={1} className="text-muted hover:text-accent transition-colors duration-500 cursor-pointer max-md:h-4 max-md:w-4" /></a>
 					</div>
 				</div>
 			</PageSection>
