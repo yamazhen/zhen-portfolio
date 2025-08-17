07303276aa1a47c0976bae26ebe04fdd99f861d0
yamazhen <bowxyt@gmail.com>
2025-08-17 17:53:32 +0900

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 	return (
 		<>
 			<PageSection first className="justify-center">
-				<div className="flex items-center gap-6 lg:gap-10 max-lg:flex-col py-8 max-lg:items-start">
+				<div className="flex items-center gap-6 lg:gap-10 max-lg:flex-col py-8 max-lg:items-start justify-center">
 					<div className="flex flex-col gap-8 max-lg:gap-4 max-md:gap-2 max-lg:order-1">
 						<small className="max-lg:text-xs"><span className="text-accent">~/portfolio</span> $ cat intro.txt</small>
 						<Typewriter text="hi, bowen here. 👋" className="text-accent max-lg:text-4xl max-md:text-base text-5xl font-bold whitespace-nowrap" />
